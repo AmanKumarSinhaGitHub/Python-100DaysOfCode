b20224a54794bebf5a678370ad9caf3016ed84e0
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-06-15 09:08:18 +0530

    Initial Commit

---
+++ Day 5 - Comments, Escape Sequence Characters and Print Statements/main.py
@@ -0,0 +1,36 @@
+# Print Statements
+
+print("Welcome to Day", 5)
+# Welcome to Day 5
+
+print("Hey", 1, 4, sep="~")
+# Hey~1~4
+
+print("Python ", end="3 \n")
+# Python 3
+
+#-----------------------------------------------------------------#
+
+# This is Single Line Comment
+
+""" This is Multi line Comment
+these line are not going to print
+I hope you understand """
+
+#-----------------------------------------------------------------#
+
+# \n is a escape sequence that give you a new line
+print("I am learning Python \n And I am really enjoying it")
+''' I am learning Python
+    And I am really enjoying it '''
+
+print("This will \" execute")
+# This will " execute
+
+print("This will \" execute \" ") 
+# This will " execute "
+
+print("I\'m a coder")
+# I'm a coder
+
+#-----------------------------------------------------------------#
