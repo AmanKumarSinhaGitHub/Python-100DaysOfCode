7b372d97678bbab28f5c847fd10435bef76f07a0
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-06-16 20:54:05 +0530

    Initial Commit

---
+++ Day 6 - Variable and Data Types/main.py
@@ -0,0 +1,23 @@
+a = 1
+print(a)  # Output : 1
+
+# To know the type of data type
+print(type(a)) # Output : int
+
+b = True
+print(b) # Output : True
+
+c = "Harry"
+print(c) # Output : Harry
+
+d = None 
+print(d) # Output : None
+
+listA = [8, 2.98, [4, -98], ["apple", "banana"]]
+print(listA)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Aman", "age":20, "canVote":True}
+print(dict1)
