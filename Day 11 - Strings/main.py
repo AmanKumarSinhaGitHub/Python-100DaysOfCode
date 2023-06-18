8c4ca090b11d0f6f98764ca5cd8e07dc99e11dd2
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-06-18 13:17:06 +0530

    Initial Commit

---
+++ Day 11 - Strings/main.py
@@ -0,0 +1,31 @@
+# String in Double Quote
+print("Aman")
+
+# String in Single Quote
+print('Python')
+
+# String in Double Quote within Single Quote
+print('I am Learning "Python". It is really amazing.')
+
+# Multiline String
+print("""Hello,
+I am Aman.
+I am Learing Python.""")
+
+# Accessing Character of a String
+
+name = "Python"
+print(name[0]) # Output : P
+print(name[1]) # Output : y
+print(name[2]) # Output : t
+print(name[3]) # Output : h
+print(name[4]) # Output : o
+print(name[5]) # Output : n
+
+# Note : Index start from zero (0)
+
+# Accessing String characters using LOOPS
+print("Printing the same thing using LOOPS")
+for character in name:
+    print(character)
+    
