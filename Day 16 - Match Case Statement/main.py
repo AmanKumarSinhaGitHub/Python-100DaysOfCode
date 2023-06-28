a65546757332d816edef8a3f85bd0447876912b1
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-06-28 19:58:09 +0530

    Initial Commit

---
+++ Day 16 - Match Case Statement/main.py
@@ -0,0 +1,26 @@
+x = 4
+# x is the variable to match
+match x:
+    # if x is 0
+    case 0:
+        print("x is zero")
+    # case with if-condition
+    case 4 if x % 2 == 0:
+        print("x % 2 == 0 and case is 4")
+    # Empty case with if-condition
+    case _ if x < 10:
+        print("x is < 10")
+    # default case(will only be matched if the above cases were not matched)
+    # so it is basically just an else:
+    case _:
+        print(x)
+
+
+day = "Tuesday"
+
+match day:
+    case "Monday"  :
+        print("It's Monday")
+    
+    case "Tuesday" :
+        print("It's Tuesday")
