1153b6270102589ddf860e236408f99c5a0a0f8e
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-07-02 14:53:21 +0530

    Python-100DaysOfCode

---
+++ Day 020 - Functions/main.py
@@ -0,0 +1,17 @@
+# Function 
+def findGreater(a, b):
+    if(a>b):
+        print(a, "is Greater than", b)
+    elif(b>a):
+        print(b, "is Greater than", a)
+    else:
+        print(a, "and", b, "are Equal")
+
+
+num1 = 10
+num2 = 20
+findGreater(num1, num2)
+
+# Empty Function
+def findSmaller(a,b):
+    pass
