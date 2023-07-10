94e4b197832c7a8d2dea53da9372f488b541cfc5
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-07-10 21:45:15 +0530

    Python-100DaysOfCode

---
+++ Day 024 - Tuples in Python/main.py
@@ -0,0 +1,16 @@
+tup = (1,4,6, "Hello")
+print(type(tup)) # <class 'tuple'>
+print(tup) #v (1, 4, 6, 'Hello')
+
+
+tp = (1)
+print(type(tp)) # <class 'int'>
+# So If you want to create tuple with one elemenent, You have to add a comma at the last.
+tp = (1,)
+print(type(tp)) # <class 'tuple'>
+
+#Access tuple element
+tuple = (10, "Hey", 20, "Aman")
+print(tuple[1], tuple[3]) # Hey Aman
+
+# All operation is same just like list
