fbacb0844d11dffd09b46ccdd480c1a0e0702528
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-07-16 17:17:02 +0530

    Python-100DaysOfCode

---
+++ Day 030 - Recursion/main.py
@@ -0,0 +1,22 @@
+# factorial(5) = 5*4*3*2*1
+# factorial(4) = 4*3*2*1
+# factorial(3) = 3*2*1
+# factorial(2) = 2*1
+# factorial(1) = 1
+# factorial(0) = 1
+
+
+# factorial(n) = n * factorial(n-1)
+def factorial(n):
+  if (n == 0 or n == 1):
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+# 5 * factorial(4)
+# 5 * 4 * factorial(3)
+# 5 * 4 * 3 * factorial(2)
+# 5 * 4 * 3 * 2 * factorial(1)
+# 5 * 4 * 3 * 2 * 1
