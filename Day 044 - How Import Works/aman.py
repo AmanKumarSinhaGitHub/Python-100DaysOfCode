da6b54afc6be5c62f5c59f4f9be55b2f2748d3c0
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-08-08 20:03:03 +0530

    Python-100DaysOfCode

---
+++ Day 044 - How Import Works/aman.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Hello, How are You?")
+    name = input("Tell me you name : ")
+    print("Hello", name)
+
+def bye():
+    print("Bye! Nice to meet you")
--- Day 044 - How Import Works/main.py
+++ Day 044 - How Import Works/main.py
@@ -5,3 +5,11 @@ print(math.sqrt(49)) # 7.0
 print(math.pi) # 3.141592653589793
 
 # explore more from README.md file
+
+
+
+
+# importing function from aman.py file
+from aman import *
+welcome()
+bye()
