01e6e5b45ec3b4ee859f44c39b88900e90bbd84d
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-08-31 20:04:18 +0530

    Python-100DaysOfCode

--- Day 059 - Decorators in Python/main.py
+++ Day 059 - Decorators in Python/main.py
@@ -1,3 +1,12 @@
+# Decorators in Python are functions that modify the behavior of other functions.
+
+# They are often used to add functionality to existing functions without changing their source code.
+
+# Decorators are applied using the "@" symbol above a function definition. 
+
+# They receive the original function as an argument, and usually return a new function that enhances or alters the behavior of the original function.
+
+
 def greet(fx):
     
     def mfx():
@@ -13,4 +22,7 @@ def hello():
     print("Hello World, I am Aman")
 
 hello()
+
+
+
 
