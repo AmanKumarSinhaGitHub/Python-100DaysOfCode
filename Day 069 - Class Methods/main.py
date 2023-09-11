ade3e2ce26bf64d39c4ca8d7492fe505fd80c3cd
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-09-11 19:47:47 +0530

    Python-100DaysOfCode

---
+++ Day 069 - Class Methods/main.py
@@ -0,0 +1,22 @@
+class Employee:
+
+  company = "Apple"
+
+  def show(self):
+    print(f"The name is {self.name} and company is {self.company}")
+
+  @classmethod
+  def changeCompany(cls, newCompany):
+    cls.company = newCompany
+
+
+e1 = Employee()
+e1.name = "Harry"
+e1.show()
+e1.changeCompany("Tesla")
+e1.show()
+print(Employee.company)
+
+e2 = Employee()
+e2.name = "Rohan"
+e2.show()
